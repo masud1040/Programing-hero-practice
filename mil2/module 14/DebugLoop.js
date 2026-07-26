6f620bc4d85730eb5a38195e49de87db31ddc920
masud1040 <saifulalammasudmn@gmail.com>
2026-07-26 18:49:26 +0600

    loop

---
+++ mil2/module 14/DebugLoop.js
@@ -0,0 +1,31 @@
+const numbers = [2, 5, 8, 11, 14, 17, 20];
+let sum = 0;
+
+//for (let i = 0; i <= numbers.length; i++) { 
+for (let i = 0; i < numbers.length; i++) { 
+  if (numbers[i] % 2 === 0) {
+    sum += numbers[i];
+  }
+}
+
+console.log(sum);
+
+// Expected Output:
+// 44
+
+
+const text = "javascript";
+let count = 0;
+
+const vowels = ["a", "e", "i", "o", "u"];
+
+for (let i = 0; i < text.length; i++) {
+  if (vowels.includes(text[i])) {
+    count++;
+  }
+}
+
+console.log(count);
+
+// Expected Output:
+// 3
