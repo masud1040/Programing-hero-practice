7a7f796edbe24bc98185851cd7c7e5f96640fcb9
masud1040 <saifulalammasudmn@gmail.com>
2026-07-26 19:29:52 +0600

    array

--- mil2/module 14/DebugLoop.js
+++ mil2/module 14/DebugLoop.js
@@ -1,7 +1,7 @@
 const numbers = [2, 5, 8, 11, 14, 17, 20];
 let sum = 0;
 
-//for (let i = 0; i <= numbers.length; i++) { 
+//for (let i = 0; i <= numbers.length; i++) {     number.length -1 
 for (let i = 0; i < numbers.length; i++) { 
   if (numbers[i] % 2 === 0) {
     sum += numbers[i];
@@ -20,6 +20,7 @@ let count = 0;
 const vowels = ["a", "e", "i", "o", "u"];
 
 for (let i = 0; i < text.length; i++) {
+  //if (vowels.includes(text)) {   bug  
   if (vowels.includes(text[i])) {
     count++;
   }
---
+++ mil2/module 14/array.js
@@ -0,0 +1,4 @@
+function  Arr (arr){
+    return arr.slice(arr.length-2,arr.length); // -1  dile  output asbe na 
+}
+console.log(Arr([1,2,3,4,5,6,7]));
