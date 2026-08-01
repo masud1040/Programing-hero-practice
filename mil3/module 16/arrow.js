9bfe197e5ec773cba70d370b53dc91578648f54e
masud1040 <saifulalammasudmn@gmail.com>
2026-08-01 20:41:50 +0600

    arrow function

---
+++ mil3/module 16/arrow.js
@@ -0,0 +1,16 @@
+const sum = (a, b) => {
+  return a + b;
+};
+// console.log(sum(10));
+
+
+const stu = {
+    name:"masud",
+    roll: 39838,
+
+    showifno :function(){
+        return stu.name
+    }
+}
+
+console.log(stu.showifno());
--- mil3/module 16/spread.js
+++ mil3/module 16/spread.js
@@ -3,6 +3,16 @@ let height = Math.max(...array);
 let minimum = Math.min(...array);
 
 
-console.log(height);
-console.log(minimum);
+// console.log(height);
+// console.log(minimum);
 
+
+function sum (...rest){
+    let sum =0;
+    for (const element of rest) {
+        sum+= element;
+    }
+    console.log(sum);
+    
+}
+sum(1,2,3,4,5,6,7,8,9,10,11);
